8c9eabf149953a3735308da6b724159337cba363
Avalon <Avalon>
2024-02-22 12:54:01 +0800

    chore: das-server.sh

--- das-server.sh
+++ das-server.sh
@@ -9,9 +9,24 @@ PARENT_CHAIN_NODE_URL=$(jq -r '.node."data-availability"."parent-chain-node-url"
 # Check if the "data-availability" key exists
 if jq -e '.node | has("data-availability")' /home/user/.arbitrum/nodeConfig.json >/dev/null; then
     # Start daserver with the new command
-    /usr/local/bin/daserver --data-availability.parent-chain-node-url "$PARENT_CHAIN_NODE_URL" --enable-rpc --rpc-addr '0.0.0.0' --enable-rest --rest-addr '0.0.0.0' --log-level 3 --data-availability.local-db-storage.enable --data-availability.local-db-storage.data-dir /home/user/das-data --data-availability.local-db-storage.discard-after-timeout=false --data-availability.key.key-dir /home/user/.arbitrum/keys --data-availability.local-cache.enable --data-availability.sequencer-inbox-address $SEQUENCER_INBOX_ADDRESS
+    /usr/local/bin/daserver \
+    --data-availability.parent-chain-node-url "$PARENT_CHAIN_NODE_URL" \
+    --enable-rpc --rpc-addr '0.0.0.0' \
+    --enable-rest --rest-addr '0.0.0.0' \
+    --log-level 3 \
+    --data-availability.local-db-storage.enable \
+    --data-availability.local-db-storage.data-dir /home/user/das-data \
+    --data-availability.local-db-storage.discard-after-timeout=false \
+    --data-availability.key.key-dir /home/user/.arbitrum/keys \
+    --data-availability.local-cache.enable \
+    --data-availability.sequencer-inbox-address $SEQUENCER_INBOX_ADDRESS
 else
     # Log a message and exit if "data-availability" key doesn't exist
     echo "You're running in Rollup mode. No need for das-server, so exiting the container ..."
     exit
 fi
+
+
+# Archive DA server config, local-file-storage and ipfs-storage don't discard data after expiring, so the option discard-after-timeout is not available.
+# --data-availability.s3-storage.discard-after-timeout=false
+# --data-availability.local-db-storage.discard-after-timeout=false
